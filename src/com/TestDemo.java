d2234cd6c8cf33bf08dbeed49d4222f58db71b18
GB2071FR <GB2071FR@ashuang.com>
2023-05-08 21:11:17 +0800

    Idea second commit

--- src/com/TestDemo.java
+++ src/com/TestDemo.java
@@ -3,5 +3,7 @@ package com;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
